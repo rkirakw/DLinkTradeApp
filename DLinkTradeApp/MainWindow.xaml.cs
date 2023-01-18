fda6ac7d1b95f1b8bc6c5346a7f7493432000a19
rkirakw <inheritedldvb@gmail.com>
2023-01-18 11:40:34 +0300

    Changerd

--- DLinkTradeApp/MainWindow.xaml.cs
+++ DLinkTradeApp/MainWindow.xaml.cs
@@ -1,42 +1,29 @@
 ﻿using DLinkTradeApp.HighLevelPages;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DLinkTradeApp {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window {
         public static MainWindow _instance { get; private set; }
         public MainWindow() {
             InitializeComponent();
             _instance = this;
 
-            //TODO Заменить на LogPage.get!!!
+            // Обращение к WorkPage.get для вызова статического конструктора
             var _temp = WorkPage.get;
+            // Переход на страницу авторизации
             screenFrame.Navigate(LogPage.get);
         }
 
+        // Подавить событие, если была нажата клавиша Tab
         private void Page_PreviewKeyDown(object sender, KeyEventArgs e) {
-            var obj = (e.Source as FrameworkElement)?.Tag ?? "__none";
-            if ((obj.Equals("__none") || !obj.Equals("__handle_tab")) && e.Key == Key.Tab)
-                e.Handled = true;
+            if(e.Key == Key.Tab)
+               e.Handled = true;
         }
 
+        // Закрыть соединение с базой данных
         private void Window_Closed(object sender, EventArgs e) {
-            //Закрытие соединения с БД
             Manager.GetConnection.Close();
         }
     }
